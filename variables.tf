220d7d525c6a92558f4c4bf308d566ef2af878f3
PavanKumarAnkam <pa1kumarankam@gmail.com>
2024-08-27 10:37:56 +0530

    latest

---
+++ variables.tf
@@ -0,0 +1,55 @@
+#### Project ####
+variable "project_name"{
+    type = string
+}
+
+variable "environment"{
+    type = string
+    default = "dev"
+}
+
+variable "common_tags"{
+    type = map
+}
+
+#### VPC ####
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "enable_dns_hostnames" {
+    type = bool
+    default = true
+}
+
+variable "vpc_tags" {
+    type = map
+    default = {}
+}
+
+### igw ###
+variable "igw_tags" {
+    type=map 
+    default = {}
+}
+
+### public subnet ###
+variable "public_subnet_cidrs" {
+  type = list
+  validation {
+      condition = length(var.public_subnet_cidrs) == 2
+      error_message = "pls provide only 2 valid public subnet cidr"
+  } 
+}
+
+variable "public_subnet_cidr_tags" {
+    type=map 
+    default = {}
+}
+
+
+variable "nat_gateway_tags" {
+  type = map
+  default = {}
+}
---
+++ vpc.tf
@@ -0,0 +1,88 @@
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  tags = merge(
+    var.common_tags,
+    var.vpc_tags,
+    {
+        Name = local.resource_name
+    }
+  )
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.igw_tags,
+    {
+      Name=local.resource_name
+    }
+  )
+}
+
+## basic template - public subnet
+# resource "aws_subnet" "main" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.0.1.0/24"
+
+#   tags = {
+#     Name = "Main"
+#   }
+# }
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+
+  tags = merge(
+    var.common_tags,
+    var.public_subnet_cidr_tags,
+    {
+      Name="${local.resource_name}-${local.az_names[count.index]}"
+    }
+  )
+}
+
+# resource "aws_eip" "lb" {
+#   instance = aws_instance.web.id
+#   domain   = "vpc"
+# }
+
+resource "aws_eip" "elastic" {
+  domain   = "vpc"
+}
+
+# resource "aws_nat_gateway" "example" {
+#   allocation_id = aws_eip.example.id
+#   subnet_id     = aws_subnet.example.id
+
+#   tags = {
+#     Name = "gw NAT"
+#   }
+
+#   # To ensure proper ordering, it is recommended to add an explicit dependency
+#   # on the Internet Gateway for the VPC.
+#   depends_on = [aws_internet_gateway.example]
+# }
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.elastic.id
+  subnet_id     = aws_subnet.public[0].id # public[0] refers to subnet "us-east-1a", public[1] = us-east-1b
+
+  tags = merge(
+      var.common_tags,
+      var.nat_gateway_tags,
+  {
+      Name="${local.resource_name}"
+    }
+  )
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
+}
